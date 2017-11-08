d55c81d8fec9b883e5394ee7de890573e8a258f2
Deetss <dylan817@live.com>
2017-11-08 21:54:30 +0000

    Setup very basic signup form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+    
+    def new
+        @user = User.new
+    end
+    
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:success] = "Welcome to Brite Event!"
+            redirect_to show_user_path
+        else
+            flash.now[:danger] = "Sign up failed!"
+            render :new
+        end
+    end
+    
+    def show
+    end
+    
+    private
+    
+    def user_params
+        params.require(:user).permit(:name, :email)
+    end
+end
