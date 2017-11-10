b31f00faebdee203beb31e29b5a87af29aa3f4d2
Deetss <dylan817@live.com>
2017-11-10 19:52:33 +0000

    Styles everything

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,14 +6,17 @@ class SessionsController < ApplicationController
         @user = User.find_by(email: params[:session][:email])
         if @user
             log_in @user
+            flash[:success] = "Log in successful!"
             redirect_to root_url
         else
+            flash.now[:danger] = "Email not found!"
             render :new
         end
     end
     
     def destroy
         log_out
+        flash[:warning] = "Log out successful!"
         redirect_to root_url
     end
 end
