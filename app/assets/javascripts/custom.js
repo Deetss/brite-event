dcad15984b737e518c3990f839975327016289d6
Deetss <dylan817@live.com>
2017-11-13 20:46:42 +0000

    Adjusts some css to make things cleaner, makes events into cards

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -22,6 +22,14 @@ document.addEventListener("turbolinks:load", function(){
         ampmclickable: true, // make AM PM clickable
         aftershow: function(){} //Function for after opening timepicker
     });
+    
+    $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens,
+    }
+  );
 })
 
 
